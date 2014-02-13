b684f457d9a10fe4a9bd6c4607f35cd42ca002b1
Felix Pahlow <felixgruenschnabel@yahoo.de>
2014-02-13 13:27:01 +0100

    Third party libraries part I
    Ketcher is Copyright (C) of GGA Software Services LLC 2009-2010.
    FileSaver.js 2013-10-21 By Eli Grey.
    Base64 Copyright (C) 1999 Masanao Izumo.

---
+++ qt/resources/lib/base64.js
@@ -0,0 +1,306 @@
+/* Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
+ * Version: 1.0
+ * LastModified: Dec 25 1999
+ * This library is free.  You can redistribute it and/or modify it.
+ */
+
+/*
+ * Interfaces:
+ * b64 = base64encode(data);
+ * data = base64decode(b64);
+ */
+
+(function() {
+
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+
+function base64encode(str) {
+    var out, i, len;
+    var c1, c2, c3;
+
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+	c1 = str.charCodeAt(i++) & 0xff;
+	if(i == len)
+	{
+	    out += base64EncodeChars.charAt(c1 >> 2);
+	    out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+	    out += "==";
+	    break;
+	}
+	c2 = str.charCodeAt(i++);
+	if(i == len)
+	{
+	    out += base64EncodeChars.charAt(c1 >> 2);
+	    out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+	    out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+	    out += "=";
+	    break;
+	}
+	c3 = str.charCodeAt(i++);
+	out += base64EncodeChars.charAt(c1 >> 2);
+	out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+	out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+	out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+
+function base64decode(str) {
+    var c1, c2, c3, c4;
+    var i, len, out;
+
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+	/* c1 */
+	do {
+	    c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+	} while(i < len && c1 == -1);
+	if(c1 == -1)
+	    break;
+
+	/* c2 */
+	do {
+	    c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+	} while(i < len && c2 == -1);
+	if(c2 == -1)
+	    break;
+
+	out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+
+	/* c3 */
+	do {
+	    c3 = str.charCodeAt(i++) & 0xff;
+	    if(c3 == 61)
+		return out;
+	    c3 = base64DecodeChars[c3];
+	} while(i < len && c3 == -1);
+	if(c3 == -1)
+	    break;
+
+	out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+
+	/* c4 */
+	do {
+	    c4 = str.charCodeAt(i++) & 0xff;
+	    if(c4 == 61)
+		return out;
+	    c4 = base64DecodeChars[c4];
+	} while(i < len && c4 == -1);
+	if(c4 == -1)
+	    break;
+	out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+}
+
+if (!window.btoa) window.btoa = base64encode;
+if (!window.atob) window.atob = base64decode;
+window.Base64 = {
+	encode: window.btoa,
+	decode: window.atob
+};
+
+})();
+
+
+// Next part. Source:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
+// "Any copyright is dedicated to the Public Domain. http://creativecommons.org/publicdomain/zero/1.0/"
+/*
+ * @author fusionchess <https://developer.mozilla.org/en-US/profiles/fusionchess>
+ */
+
+ (function(w) {
+	"use strict";
+
+	/*\
+	|*|
+	|*|  Base64 / binary data / UTF-8 strings utilities
+	|*|
+	|*|  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Base64_encoding_and_decoding
+	|*|
+	\*/
+
+	/* Array of bytes to base64 string decoding */
+
+	function b64ToUint6 (nChr) {
+
+	  return nChr > 64 && nChr < 91 ?
+			nChr - 65
+		 : nChr > 96 && nChr < 123 ?
+			nChr - 71
+		 : nChr > 47 && nChr < 58 ?
+			nChr + 4
+		 : nChr === 43 ?
+			62
+		 : nChr === 47 ?
+			63
+		 :
+			0;
+
+	}
+	w.b64ToUint6 = w.b64ToUint6 || b64ToUint6;
+
+	function base64DecToArr (sBase64, nBlocksSize) {
+
+	  var
+		 sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
+		 nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2, taBytes = new Uint8ClampedArray(nOutLen);
+
+	  for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+		 nMod4 = nInIdx & 3;
+		 nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+		 if (nMod4 === 3 || nInLen - nInIdx === 1) {
+			for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+			  taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+			}
+			nUint24 = 0;
+
+		 }
+	  }
+
+	  return taBytes;
+	}
+	w.base64DecToArr = w.base64DecToArr || base64DecToArr;
+
+	/* Base64 string to array encoding */
+
+	function uint6ToB64 (nUint6) {
+
+	  return nUint6 < 26 ?
+			nUint6 + 65
+		 : nUint6 < 52 ?
+			nUint6 + 71
+		 : nUint6 < 62 ?
+			nUint6 - 4
+		 : nUint6 === 62 ?
+			43
+		 : nUint6 === 63 ?
+			47
+		 :
+			65;
+
+	}
+	w.uint6ToB64 = w.uint6ToB64 || uint6ToB64;
+
+	function base64EncArr (aBytes) {
+
+	  var nMod3, sB64Enc = "";
+
+	  for (var nLen = aBytes.length, nUint24 = 0, nIdx = 0; nIdx < nLen; nIdx++) {
+		 nMod3 = nIdx % 3;
+		 if (nIdx > 0 && (nIdx * 4 / 3) % 76 === 0) { sB64Enc += "\r\n"; }
+		 nUint24 |= aBytes[nIdx] << (16 >>> nMod3 & 24);
+		 if (nMod3 === 2 || aBytes.length - nIdx === 1) {
+			sB64Enc += String.fromCharCode(uint6ToB64(nUint24 >>> 18 & 63), uint6ToB64(nUint24 >>> 12 & 63), uint6ToB64(nUint24 >>> 6 & 63), uint6ToB64(nUint24 & 63));
+			nUint24 = 0;
+		 }
+	  }
+
+	  return sB64Enc.replace(/A(?=A$|$)/g, "=");
+
+	}
+	w.base64EncArr = w.base64EncArr || base64EncArr;
+
+	/* UTF-8 array to DOMString and vice versa */
+
+	function UTF8ArrToStr (aBytes) {
+
+	  var sView = "";
+
+	  for (var nPart, nLen = aBytes.length, nIdx = 0; nIdx < nLen; nIdx++) {
+		 nPart = aBytes[nIdx];
+		 sView += String.fromCharCode(
+			nPart > 251 && nPart < 254 && nIdx + 5 < nLen ? /* six bytes */
+			  /* (nPart - 252 << 32) is not possible in ECMAScript! So...: */
+			  (nPart - 252) * 1073741824 + (aBytes[++nIdx] - 128 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+			: nPart > 247 && nPart < 252 && nIdx + 4 < nLen ? /* five bytes */
+			  (nPart - 248 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+			: nPart > 239 && nPart < 248 && nIdx + 3 < nLen ? /* four bytes */
+			  (nPart - 240 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+			: nPart > 223 && nPart < 240 && nIdx + 2 < nLen ? /* three bytes */
+			  (nPart - 224 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+			: nPart > 191 && nPart < 224 && nIdx + 1 < nLen ? /* two bytes */
+			  (nPart - 192 << 6) + aBytes[++nIdx] - 128
+			: /* nPart < 127 ? */ /* one byte */
+			  nPart
+		 );
+	  }
+
+	  return sView;
+
+	}
+	w.UTF8ArrToStr = w.UTF8ArrToStr || UTF8ArrToStr;
+
+	function strToUTF8Arr (sDOMStr) {
+
+	  var aBytes, nChr, nStrLen = sDOMStr.length, nArrLen = 0;
+
+	  /* mapping... */
+
+	  for (var nMapIdx = 0; nMapIdx < nStrLen; nMapIdx++) {
+		 nChr = sDOMStr.charCodeAt(nMapIdx);
+		 nArrLen += nChr < 0x80 ? 1 : nChr < 0x800 ? 2 : nChr < 0x10000 ? 3 : nChr < 0x200000 ? 4 : nChr < 0x4000000 ? 5 : 6;
+	  }
+
+	  aBytes = new Uint8Array(nArrLen);
+
+	  /* transcription... */
+
+	  for (var nIdx = 0, nChrIdx = 0; nIdx < nArrLen; nChrIdx++) {
+		 nChr = sDOMStr.charCodeAt(nChrIdx);
+		 if (nChr < 128) {
+			/* one byte */
+			aBytes[nIdx++] = nChr;
+		 } else if (nChr < 0x800) {
+			/* two bytes */
+			aBytes[nIdx++] = 192 + (nChr >>> 6);
+			aBytes[nIdx++] = 128 + (nChr & 63);
+		 } else if (nChr < 0x10000) {
+			/* three bytes */
+			aBytes[nIdx++] = 224 + (nChr >>> 12);
+			aBytes[nIdx++] = 128 + (nChr >>> 6 & 63);
+			aBytes[nIdx++] = 128 + (nChr & 63);
+		 } else if (nChr < 0x200000) {
+			/* four bytes */
+			aBytes[nIdx++] = 240 + (nChr >>> 18);
+			aBytes[nIdx++] = 128 + (nChr >>> 12 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 6 & 63);
+			aBytes[nIdx++] = 128 + (nChr & 63);
+		 } else if (nChr < 0x4000000) {
+			/* five bytes */
+			aBytes[nIdx++] = 248 + (nChr >>> 24);
+			aBytes[nIdx++] = 128 + (nChr >>> 18 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 12 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 6 & 63);
+			aBytes[nIdx++] = 128 + (nChr & 63);
+		 } else /* if (nChr <= 0x7fffffff) */ {
+			/* six bytes */
+			aBytes[nIdx++] = 252 + /* (nChr >>> 32) is not possible in ECMAScript! So...: */ (nChr / 1073741824);
+			aBytes[nIdx++] = 128 + (nChr >>> 24 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 18 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 12 & 63);
+			aBytes[nIdx++] = 128 + (nChr >>> 6 & 63);
+			aBytes[nIdx++] = 128 + (nChr & 63);
+		 }
+	  }
+
+	  return aBytes;
+
+	}
+	w.strToUTF8Arr = w.strToUTF8Arr || strToUTF8Arr;
+}(window));
